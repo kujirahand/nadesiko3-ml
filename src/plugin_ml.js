132d0e9465b0bbdd1b4733de764fee3d3184e714
kujirahand <web@kujirahand.com>
2019-12-27 00:28:00 +0800

    SVM and RandomForest supported

---
+++ src/plugin_ml.js
@@ -0,0 +1,189 @@
+// plugin_ml.js
+PLUGIN_ML_INIT_ERROR = "『SVM開』や『ランダムフォレスト開』などでオブジェクトを初期化してください。"
+const PluginML = {
+  '初期化': {
+    type: 'func',
+    josi: [],
+    fn: function (sys) {
+      sys.__ml = null
+      sys.__ml_load = null
+      sys.__ml_toString = null
+    }
+  },
+  // @機械学習
+  'SVM開': { // @機械学習のアルゴリズムSVMをパラメータOPTIONで開く。 OPTION={type,kernel,gamma,cost}// @SVMひらく
+    type: 'func',
+    josi: [['で']],
+    fn: function (option, sys) {
+      const SVM = require('libsvm-js/asm')
+      // option
+      if (option.type) { // default: C_SVC
+      	option.type = SVM.SVM_TYPES[option.type]
+      }
+      if (option.kernel) { // default: RBF
+      	option.kernel = SVM.KERNEL_TYPES[option.kernel]
+      }
+      const svm = new SVM(option)
+      sys.__ml = svm
+      sys.__ml_load = SVM.load
+      sys.__ml_toString = function () {
+      	return sys.__ml.serializeModel()
+      }
+      return sys.__ml
+    }
+  },
+  'ランダムフォレスト開': { // @機械学習のアルゴリズムRandom ForestをOPTIONで開く。OPTION={seed,maxFeatures,replacement=(true|false),nEstimators} // @らんだむふぉれすとひらく
+    type: 'func',
+    josi: [['で']],
+    fn: function (option, sys) {
+      const RF = require('ml-random-forest').RandomForestClassifier
+      const rf = new RF(option)
+      sys.__ml = rf
+      sys.__ml_load = function (str) {
+      	const model = JSON.parse(str)
+      	return RF.load(model)
+      }
+      sys.__ml_toString = () => {
+      	const j = sys.__ml.toJSON()
+      	const s = JSON.stringify(j)
+      	return s
+      }
+      return sys.__ml
+    }
+  },
+  'ランダムフォレスト回帰開': { // @機械学習のアルゴリズムRandom Forestの回帰をパラメータPARAMSで開く // @らんだむふぉれすとかいきひらく
+    type: 'func',
+    josi: [['で']],
+    fn: function (params, sys) {
+      const RF = require('ml-random-forest').RandomForestRegression
+      const rf = new RF(params)
+      sys.__ml_load = function (str) {
+      	const model = JSON.parse(str)
+      	return RF.load(model)
+      }
+      sys.__ml_toString = () => {
+      	const j = sys.__ml.toJSON()
+      	const s = JSON.stringify(j)
+      	return s
+      }
+      return sys.__ml
+    }
+  },
+  '学習': { // @入力配列inputsとラベル配列labelsで機械学習する // @がくしゅう
+    type: 'func',
+    josi: [['と'], ['で']],
+    fn: function (inputs, labels, sys) {
+      if (!sys.__ml || !sys.__ml.train) {
+      	throw new Error('『学習』にて' + PLUGIN_ML_INIT_ERROR)
+      }
+      sys.__ml.train(inputs, labels) 
+    },
+    return_none: true
+  },
+  '予測': { // @入力配列inputsで予測し機械学習の結果を配列で返す // @よそく
+    type: 'func',
+    josi: [['で']],
+    fn: function (inputs, sys) {
+      if (!sys.__ml || !sys.__ml.predict) {
+      	throw new Error('『予測』にて' + PLUGIN_ML_INIT_ERROR)
+      }
+      let a = sys.__ml.predict(inputs)
+      return a
+    }
+  },
+  '予測': { // @入力配列inputsで予測し機械学習の結果を配列で返す // @よそく
+    type: 'func',
+    josi: [['で']],
+    fn: function (inputs, sys) {
+      if (!sys.__ml || !sys.__ml.predict) {
+      	throw new Error('『予測』にて' + PLUGIN_ML_INIT_ERROR)
+      }
+      let a = sys.__ml.predict(inputs)
+      return a
+    }
+  },
+  'アヤメデータ取得': { // @アヤメデータをJSON形式で取得して返す // @あやめでーたしゅとく
+    type: 'func',
+    josi: [],
+    fn: function (sys) {
+      const a = require('./iris.json')
+      return {
+      	'データ': a['data'], 
+      	'ラベル': a['label'], 
+      	'ラベル名': a['name']
+      }
+    }
+  },
+  'モデル取得': { // @現在学習済みのモデルを取得する // @もでるしゅとく
+    type: 'func',
+    josi: [],
+    fn: function (sys) {
+      if (!sys.__ml_toString) {
+      	throw new Error('『モデル設定』にて' + PLUGIN_ML_INIT_ERROR)
+      }
+      return sys.__ml_toString()
+    }
+  },
+  'モデル設定': { // @現在学習済みのモデルMを設定する // @もでるせってい
+    type: 'func',
+    josi: [['を', 'の']],
+    fn: function (m, sys) {
+      if (!sys.__ml_load) {
+      	throw new Error('『モデル設定』にて' + PLUGIN_ML_INIT_ERROR)
+      }
+      sys.__ml = sys.__ml_load(m)
+      return sys.__ml
+    }
+  },
+  'データランダム分割': { // @データとラベルの組をRATEで分割して{学習:{データ:[..],ラベル:[..]},テスト:{データ:[..],ラベル:[..]}}の形式で返す // @でーたらんだむぶんかつ
+    type: 'func',
+    josi: [['と'], ['を'], ['で']],
+    fn: function (data, label, rate, sys) {
+      // 組み合わせる
+      const a = []
+      for (let i = 0; i < data.length; i++) {
+      	a[i] = {'data': data[i], 'label': label[i]}
+      }
+      // シャッフル
+      a.sort( () => Math.random() - 0.5 )
+      // レートで分ける
+      if (rate >= 1.0) {
+      	rate = 0.75 // デフォルト値
+      }
+      const n = Math.floor(a.length * rate)
+      // 分割
+      res = {'学習':{'データ':[],'ラベル':[]}, 'テスト':{'データ':[], 'ラベル':[]}}
+      resTrain = res['学習']
+      resTest = res['テスト']
+      for (let i = 0; i < a.length; i++) {
+      	const fdata = a[i]['data']
+      	const flabel = a[i]['label']
+      	if (i < n) {
+      	  resTrain['データ'].push(fdata)
+      	  resTrain['ラベル'].push(flabel)
+      	} else {
+      	  resTest['データ'].push(fdata)
+      	  resTest['ラベル'].push(flabel)
+      	}
+      }
+      return res
+    }
+  },
+  '正解率計算': { // @配列T(正解)とP(予測)の正解率を計算して返す // @せいかいりつけいさん
+    type: 'func',
+    josi: [['と'], ['の', 'で']],
+    fn: function (t, p, sys) {
+      let pc = 0
+      for (let i = 0; i < t.length; i++) {
+      	if (t[i] == p[i]) {
+      		pc++
+      	}
+      }
+      return pc / t.length
+    }
+  }
+}
+
+module.exports = PluginML
+
+
---
+++ test/test-easy.js
@@ -0,0 +1,128 @@
+const assert = require('assert')
+const path = require('path')
+const nadesiko3 = require('nadesiko3')
+const NakoCompiler = nadesiko3.compiler
+const PluginNode = nadesiko3.PluginNode
+const PluginML = require('../src/plugin_ml')
+
+describe('ml_test', () => {
+  const nako = new NakoCompiler()
+  nako.addPluginObject('PluginNode', PluginNode)
+  nako.addPluginObject('PluginML', PluginML)
+  // console.log(nako.gen.plugins)
+  // nako.debug = true
+  const cmp = (code, res) => {
+    if (nako.debug) {
+      console.log('code=' + code)
+    }
+    assert.equal(nako.runReset(code).log, res)
+  }
+  const cmd = (code) => {
+    if (nako.debug) console.log('code=' + code)
+    nako.runReset(code)
+  }
+  // --- test ---
+  it('表示', () => {
+    cmp('3を表示', '3')
+  })
+  const srcJSON = "AをJSONエンコードして表示。\n"
+  const srcTrainXOR = "[[0, 0], [1, 1], [1, 0], [0, 1]]と[0,0,1,1]で学習\n"
+  it('SVM', () => {
+  	// XORテスト
+  	const srcSVM = "{}でSVM開く\n" + srcTrainXOR
+  	const train0 = srcSVM + "A=[[0,0]]で予測。\n" + srcJSON
+    cmp(train0, '[0]')
+  	const train1 = srcSVM + "A=[[0,0],[1,1],[1,0]]で予測。\n" + srcJSON
+    cmp(train1, '[0,0,1]')
+  })
+  it('SVMモデル設定', () => {
+  	const src = "{}でSVM開く\n" +
+  		srcTrainXOR +
+  		"J=モデル取得\n" +
+  		"{}でSVM開く。\n" +
+  		"Jのモデル設定\n" +
+  		"A=[[0,0],[1,0]]で予測。\n" + srcJSON
+    cmp(src, '[0,1]')
+  })
+
+  it('ランダムフォレスト', () => {
+  	// irisテスト
+  	const srcRF = "{}でランダムフォレスト開く\n" + 
+  		"Y=アヤメデータ取得。\n" +
+  		"Y['データ']とY['ラベル']で学習。\n" +
+  		"A=[[5.1,3.5,1.4,0.2],[5.9,3,5.1,1.8]]で予測。\n" + 
+  		srcJSON
+  	cmp(srcRF, "[0,2]")
+  })
+  it('ランダムフォレストモデル設定', () => {
+  	const srcRF = "{}でランダムフォレスト開く\n" + 
+  		"Y=アヤメデータ取得。\n" +
+  		"Y['データ']とY['ラベル']で学習。\n" +
+  		"J=モデル取得。\n" +
+  		"{}でランダムフォレスト開く\n"+
+  		"Jのモデル設定。\n" +
+  		"A=[[5.1,3.5,1.4,0.2],[5.9,3,5.1,1.8]]で予測。\n" + 
+  		srcJSON
+  	cmp(srcRF, "[0,2]")
+  })
+  it('正解率計算', () => {
+  	const src1 = "[1,1,1]と[1,1,1]で正解率計算して表示"
+  	const prd1 = "1"
+  	cmp(src1, prd1)
+  	const src2 = "[1,1,1,1]と[1,1,0,0]で正解率計算して表示"
+  	const prd2 = "0.5"
+  	cmp(src2, prd2)
+  })
+  it('データランダム分割', () => {
+  	const src = "" + 
+  		"Y=アヤメデータ取得。YC=Y['データ']の要素数。\n" +
+  		"R=Y['データ']とY['ラベル']を0.8でデータランダム分割\n" +
+  		"GC=R['学習']['データ']の要素数。\n" +
+  		"AC=YC*0.8;もし、GC=ACならば「OK」と表示。違えば「NG:{GC}:{AC}」を表示。"
+  	cmp(src,'OK')
+  	/*
+  	// シャッフルが偏ってしまう？
+  	const src2 = "" + 
+  		"Y=アヤメデータ取得。\n" +
+  		"R1=Y['データ']とY['ラベル']を0.8でデータランダム分割\n" +
+  		"R2=Y['データ']とY['ラベル']を0.8でデータランダム分割\n" +
+  		"A1=R1['学習']['ラベル'][0];\n" +
+  		"A2=R2['学習']['ラベル'][0];\n"+
+  		"もし、A1=A2ならば\n"+
+  		"  「NG」と表示。\n"+
+  		"違えば\n" +
+  		"　　「OK」と表示。\n" +
+  		"ここまで。"
+  	cmp(src2,'OK')
+  	*/
+  })
+  it('アヤメ予測/ランダムフォレスト', () => {
+  	const srcRF = "{}でランダムフォレスト開く\n" + 
+  		"Y=アヤメデータ取得。\n" +
+  		"R=Y['データ']とY['ラベル']を0.8でデータランダム分割\n" +
+  		// "R['学習']['データ']の要素数を表示。"+
+  		// "R['テスト']['データ']の要素数を表示。"+
+  		"R['学習']['データ']とR['学習']['ラベル']で学習。\n" +
+  		"PRED=R['テスト']['データ']で予測。\n" + 
+  		"R['テスト']['ラベル']とPREDで正解率計算してAに代入。\n" +
+  		"もし、Aが0.9以上ならば「OK」と表示。違えば「NG:{A}」と表示。"
+  	cmp(srcRF, "OK")
+  })
+  it('アヤメ予測/SVM', () => {
+  	const src = "SVM={'kernel':'RBF', 'type':'C_SVC'}でSVM開く\n" + 
+  		"Y=アヤメデータ取得。\n" +
+  		"R=Y['データ']とY['ラベル']を0.8でデータランダム分割\n" +
+  		//"R['学習']['データ']の要素数を表示。"+
+  		//"R['テスト']['データ']の要素数を表示。"+
+  		"R['学習']['データ']とR['学習']['ラベル']で学習。\n" +
+  		"PRED=R['テスト']['データ']で予測。\n" + 
+  		"R['テスト']['ラベル']とPREDで正解率計算してAに代入。\n" +
+  		"もし、Aが0.9以上ならば「OK」と表示。違えば「NG:{A}」と表示。"
+  	cmp(src, "OK")
+  })
+})
+
+
+
+
+
